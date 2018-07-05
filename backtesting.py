2d9a1ba82eb92338844a226e858c21ecd9038570
BradleyGrantham <bgrantham343@gmail.com>
2018-07-05 22:04:45 +0100

    Add kelly criterion capability and get results using it

--- backtesting.py
+++ backtesting.py
@@ -26,13 +26,16 @@ class BetTracker:
         self.pending_bet = None
         self.completed_bets = []
         self.profit = 0
+        self.bankroll = 100
 
     def make_bet(self, bet):
         self.pending_bet = bet
         self.invested += bet.stake
+        self.bankroll -= bet.stake
 
     def bet_won(self):
         self.profit += self.pending_bet.profit
+        self.bankroll += self.pending_bet.stake + self.pending_bet.profit
         self.completed_bets.append((self.pending_bet, 'W'))
         self.pending_bet = None
 
@@ -50,8 +53,12 @@ def calculate_profit(bet):
     return bet.stake * bet.odds - bet.stake
 
 
-def calculate_stake(odds, method='constant profit', constant_profit=2):
-    stake = constant_profit / (odds - 1)
+def calculate_stake(odds, method='constant profit', constant_profit=2, probability=None):
+    assert method in ['constant_profit', 'kelly']
+    if method == 'constant_profit':
+        stake = constant_profit / (odds - 1)
+    elif method == 'kelly':
+        stake = ((odds * probability) - 1) / (odds - 1)
     return stake
 
 
@@ -87,17 +94,19 @@ def main():
 
         home_odds, draw_odds, away_odds = match['info']['home odds'], match['info']['draw odds'], match['info']['away odds']
 
-        if pred_home_odds < home_odds < 3.0:
-            stake = calculate_stake(home_odds, method='2 pound profit')
-            bet = Bet(true_odds=home_odds, predicted_odds=pred_home_odds, stake=stake, profit=2, match=match, type='home')
+        if pred_home_odds < home_odds:
+            stake = calculate_stake(home_odds, probability=1 / pred_home_odds, method='kelly') * bet_tracker.bankroll
+            profit = stake * home_odds - stake
+            bet = Bet(true_odds=home_odds, predicted_odds=pred_home_odds, stake=stake, profit=profit, match=match, type='home')
             bet_tracker.make_bet(bet)
             if match['info']['home goals'] > match['info']['away goals']:
                 bet_tracker.bet_won()
             else:
                 bet_tracker.bet_lost()
-        elif pred_away_odds < away_odds < 3.0:
-            stake = calculate_stake(away_odds, method='2 pound profit')
-            bet = Bet(true_odds=away_odds, predicted_odds=pred_away_odds, stake=stake, profit=2, match=match, type='away')
+        elif pred_away_odds < away_odds:
+            stake = calculate_stake(away_odds, probability=1 / pred_away_odds, method='kelly') * bet_tracker.bankroll
+            profit = stake * away_odds - stake
+            bet = Bet(true_odds=away_odds, predicted_odds=pred_away_odds, stake=stake, profit=profit, match=match, type='away')
             bet_tracker.make_bet(bet)
             if match['info']['home goals'] < match['info']['away goals']:
                 bet_tracker.bet_won()
--- results.md
+++ results.md
@@ -7,3 +7,10 @@ Model was trained on previous 4 seasons of data with Pinnacle Sports odds
 predicted odds < given odds < 2.4 - 8.7% roi
 predicted odds < given odds - 7.3 % roi
 predicted odds < given odds < 3.0 - 6.6% roi
+
+### kelly criterio starting with £100 bankroll
+
+predicted odds < given odds < 3.0 - 14.8% roi
+predicted odds < given odds - 12.0% roi
+
+

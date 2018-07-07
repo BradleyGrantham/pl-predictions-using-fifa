61459dc27d7daebeb4809acae9cb3f0ee74504a0
BradleyGrantham <bgrantham343@gmail.com>
2018-07-07 21:07:40 +0100

    Make a deep model and backtest using new conditions. Results yet to be updated

--- backtesting.py
+++ backtesting.py
@@ -2,20 +2,16 @@
 # by using the 2017-2018 season and betting to make £2 whenever we find value
 
 from collections import namedtuple
+import os
 
 import numpy as np
 
 import constants
-from data_methods import read_match_data, read_player_data, convert_date_to_datetime_object, assign_season_to_match, assign_guids, assign_season_to_player, assign_general_position
+from data_methods import read_match_data, read_player_data, convert_date_to_datetime_object, assign_season_to_match, \
+    assign_guids, assign_season_to_player, assign_general_position
 from matching import match_lineups_to_fifa_players, create_feature_vector_from_players
 from model import NeuralNet
-
-# calculate odds based on lineup
-# if value -> place bet to make £2
-# take amount from 'bank'
-# then check score, if win, add to 'bank'
-# do this for all games
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 Bet = namedtuple('Bet', ['true_odds', 'predicted_odds', 'stake', 'type', 'profit', 'match'])
 
@@ -63,7 +59,6 @@ def calculate_stake(odds, method='constant profit', constant_profit=2, probabili
 
 
 def main():
-
     bet_tracker = BetTracker()
 
     match_data = read_match_data(season='2017-2018')
@@ -76,10 +71,18 @@ def main():
 
         print(match['info']['date'], match['info']['home team'], match['info']['away team'])
 
-        home_players_matched = match_lineups_to_fifa_players(match['info']['home lineup names'], match['info']['home lineup numbers'], match['info']['home lineup nationalities'],
-                                                             constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][match['info']['home team']], match['info']['season'], player_data)
-        away_players_matched = match_lineups_to_fifa_players(match['info']['away lineup names'], match['info']['away lineup numbers'], match['info']['away lineup nationalities'],
-                                                             constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][match['info']['away team']], match['info']['season'], player_data)
+        home_players_matched = match_lineups_to_fifa_players(match['info']['home lineup names'],
+                                                             match['info']['home lineup numbers'],
+                                                             match['info']['home lineup nationalities'],
+                                                             constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][
+                                                                 match['info']['home team']], match['info']['season'],
+                                                             player_data)
+        away_players_matched = match_lineups_to_fifa_players(match['info']['away lineup names'],
+                                                             match['info']['away lineup numbers'],
+                                                             match['info']['away lineup nationalities'],
+                                                             constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][
+                                                                 match['info']['away team']], match['info']['season'],
+                                                             player_data)
 
         home_feature_vector = create_feature_vector_from_players(home_players_matched)
         away_feature_vector = create_feature_vector_from_players(away_players_matched)
@@ -90,23 +93,26 @@ def main():
 
         probabilties = net.predict(feature_vector)
 
-        pred_home_odds, pred_draw_odds, pred_away_odds = [1/x for x in probabilties[0]]
+        pred_home_odds, pred_draw_odds, pred_away_odds = [1 / x for x in probabilties[0]]
 
-        home_odds, draw_odds, away_odds = match['info']['home odds'], match['info']['draw odds'], match['info']['away odds']
+        home_odds, draw_odds, away_odds = match['info']['home odds'], match['info']['draw odds'], match['info'][
+            'away odds']
 
-        if pred_home_odds < home_odds:
+        if pred_home_odds < home_odds < 3.2 and 0.05 <= home_odds - pred_home_odds <= 0.4:
             stake = calculate_stake(home_odds, probability=1 / pred_home_odds, method='kelly') * bet_tracker.bankroll
             profit = stake * home_odds - stake
-            bet = Bet(true_odds=home_odds, predicted_odds=pred_home_odds, stake=stake, profit=profit, match=match, type='home')
+            bet = Bet(true_odds=home_odds, predicted_odds=pred_home_odds, stake=stake, profit=profit, match=match,
+                      type='home')
             bet_tracker.make_bet(bet)
             if match['info']['home goals'] > match['info']['away goals']:
                 bet_tracker.bet_won()
             else:
                 bet_tracker.bet_lost()
-        elif pred_away_odds < away_odds:
+        elif pred_away_odds < away_odds < 3.2 and 0.05 <= away_odds - pred_away_odds >= 0.4:
             stake = calculate_stake(away_odds, probability=1 / pred_away_odds, method='kelly') * bet_tracker.bankroll
             profit = stake * away_odds - stake
-            bet = Bet(true_odds=away_odds, predicted_odds=pred_away_odds, stake=stake, profit=profit, match=match, type='away')
+            bet = Bet(true_odds=away_odds, predicted_odds=pred_away_odds, stake=stake, profit=profit, match=match,
+                      type='away')
             bet_tracker.make_bet(bet)
             if match['info']['home goals'] < match['info']['away goals']:
                 bet_tracker.bet_won()
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 
 class NeuralNet:
-    def __init__(self, hidden_nodes=8, keep_prob=1.0, learning_rate=0.01):
+    def __init__(self, hidden_nodes=8, keep_prob=1.0, learning_rate=0.001):
         self.hidden_nodes = hidden_nodes
         self.keep_prob_value = keep_prob
         self.learning_rate = learning_rate
@@ -22,19 +22,19 @@ class NeuralNet:
 
     def build_model(self):
         with self.graph.as_default():
-
             self.input = tf.placeholder(tf.float32, shape=[None, 36], name='input')
             self.target = tf.placeholder(tf.float32, shape=[None, 3], name='target')
             self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
             hidden_layer = tf.layers.dense(self.input, 16, activation=tf.nn.relu, name="hidden_layer")
-            self.output = tf.layers.dense(hidden_layer, 3, name="output")
+            hidden_layer2 = tf.layers.dense(hidden_layer, 8, activation=tf.nn.relu, name="hidden_layer2")
+            self.output = tf.layers.dense(hidden_layer2, 3, name="output")
             self.output = tf.nn.softmax(self.output, name='softmax')
 
             with tf.name_scope('losses') as scope:
                 self.loss = tf.losses.absolute_difference(self.target, self.output)
 
-                self.train = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
+                self.train = tf.train.MomentumOptimizer(self.learning_rate, 0.99).minimize(self.loss)
 
             self.training_summary = tf.summary.scalar("training_accuracy", self.loss)
             self.validation_summary = tf.summary.scalar("validation_accuracy", self.loss)
@@ -42,43 +42,42 @@ class NeuralNet:
     @staticmethod
     def init_saver(sess):
         writer = tf.summary.FileWriter('./tf-log/', sess.graph)
-        saver = tf.train.Saver()
+        saver = tf.train.Saver(max_to_keep=1)
         return writer, saver
 
     def train_model(self, X, y, X_val, y_val):
 
-        previous_val_loss = np.inf
+        best_val_loss = 0.038
 
         with tf.Session(graph=self.graph) as sess:
 
-                writer, saver = self.init_saver(sess)
+            writer, saver = self.init_saver(sess)
 
-                sess.run(tf.global_variables_initializer())
+            sess.run(tf.global_variables_initializer())
 
-                for i in range(500000):
+            for i in range(50000):
 
-                    feed_dict = {self.input: X, self.target: y, self.keep_prob: 0.95}
+                feed_dict = {self.input: X, self.target: y, self.keep_prob: 0.8}
 
-                    _, current_loss, train_sum = sess.run([self.train, self.loss, self.training_summary], feed_dict=feed_dict)
+                _, current_loss, train_sum = sess.run([self.train, self.loss, self.training_summary],
+                                                      feed_dict=feed_dict)
 
-                    if i % 10000 == 0:
-                        val_loss, val_sum = sess.run([self.loss, self.validation_summary], feed_dict={self.input: X_val, self.target: y_val, self.keep_prob: 1.0})
-                        print(i, current_loss, val_loss)
-                        writer.add_summary(val_sum, i)
-                        writer.add_summary(train_sum, i)
+                if i % 1000 == 0:
+                    val_loss, val_sum = sess.run([self.loss, self.validation_summary],
+                                                 feed_dict={self.input: X_val, self.target: y_val, self.keep_prob: 1.0})
+                    writer.add_summary(val_sum, i)
+                    writer.add_summary(train_sum, i)
 
-                        if val_loss > previous_val_loss:
-                            break
+                    print(i, current_loss, val_loss)
+                    if val_loss < best_val_loss:
+                        best_val_loss = val_loss
+                        saver.save(sess, './deep-models/deep')
 
-                        previous_val_loss = val_loss
-
-                saver.save(sess, './models/model1')
-
-    def predict(self, X, model_name='model1'):
+    def predict(self, X, model_name='deep'):
 
         with tf.Session() as sess:
-            saver = tf.train.import_meta_graph('./models/' + model_name + '.meta')
-            saver.restore(sess, './models/' + model_name)
+            saver = tf.train.import_meta_graph('./deep-models/' + model_name + '.meta')
+            saver.restore(sess, './deep-models/' + model_name)
             graph = tf.get_default_graph()
             input = graph.get_tensor_by_name('input:0')
             keep_prob = graph.get_tensor_by_name('keep_prob:0')
@@ -90,6 +89,9 @@ class NeuralNet:
 
 
 if __name__ == '__main__':
+
+    tf.set_random_seed(8)
+    np.random.seed(8)
 
     inputs = np.load('feature_vectors.npy')
     max_input = 100  # np.max(inputs)

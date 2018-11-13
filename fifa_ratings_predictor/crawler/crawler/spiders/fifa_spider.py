1efa134c76c2c63a4751acf1e85cd46498aba568
Will Franceys <wfranceys@channel4.co.uk>
2018-11-13 18:16:21 +0000

    Improving linting - apologies future git blame

--- fifa_ratings_predictor/crawler/crawler/spiders/fifa_spider.py
+++ fifa_ratings_predictor/crawler/crawler/spiders/fifa_spider.py
@@ -9,64 +9,97 @@ class FifaSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            # 'https://www.fifaindex.com/players/',
-            'https://www.fifaindex.com/players/fifa17_173/',
-            'https://www.fifaindex.com/players/fifa16_73/',
-            'https://www.fifaindex.com/players/fifa15_14/',
-            'https://www.fifaindex.com/players/fifa14_13/',
-            'https://www.fifaindex.com/players/fifa13_10/',
-
+            "https://www.fifaindex.com/players/fifa17_173/",
+            "https://www.fifaindex.com/players/fifa16_73/",
+            "https://www.fifaindex.com/players/fifa15_14/",
+            "https://www.fifaindex.com/players/fifa14_13/",
+            "https://www.fifaindex.com/players/fifa13_10/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for row in response.css('tr td'):
-            link = row.css('a::attr(href)').extract()
+        for row in response.css("tr td"):
+            link = row.css("a::attr(href)").extract()
             # print(name, link)
-            if link: 
-                if '/player/' in link[0]:
+            if link:
+                if "/player/" in link[0]:
                     url = response.urljoin(link[0])
                     yield scrapy.Request(url, callback=self.parse_player)
 
-        next_page = response.css('li.next a::attr(href)').extract_first()
+        next_page = response.css("li.next a::attr(href)").extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
-    def parse_player(self, response):
-        name = response.css('div.media-body').css('h2.media-heading::text').extract()[0]
+    @staticmethod
+    def parse_player(response):
+        name = (response
+                .css("div.media-body")
+                .css("h2.media-heading::text")
+                .extract()[0])
 
         try:
-            team = response.css('div.col-lg-4').css('div.panel-heading').css('a::attr(title)')[2].extract()
+            team = (
+                response.css("div.col-lg-4")
+                .css("div.panel-heading")
+                .css("a::attr(title)")[2]
+                .extract()
+            )
         except IndexError:
-            team = response.css('div.col-lg-4').css('div.panel-heading').css('a::attr(title)')[0].extract()
+            team = (
+                response.css("div.col-lg-4")
+                .css("div.panel-heading")
+                .css("a::attr(title)")[0]
+                .extract()
+            )
 
-        number = response.css('div.col-lg-4').css('div.panel-body').css('span.pull-right')[3].css(
-                'span::text').extract()[0]
+        number = (
+            response.css("div.col-lg-4")
+            .css("div.panel-body")
+            .css("span.pull-right")[3]
+            .css("span::text")
+            .extract()[0]
+        )
 
         if len(number) == 4:
-            number = response.css('div.col-lg-4').css('div.panel-body').css('span.pull-right')[1].css(
-                'span::text').extract()[0]
+            number = (
+                response.css("div.col-lg-4")
+                .css("div.panel-body")
+                .css("span.pull-right")[1]
+                .css("span::text")
+                .extract()[0]
+            )
 
-        position = response.css('div.col-lg-5').css('div.panel-body').css('span.label::text').extract_first()
+        position = (
+            response.css("div.col-lg-5")
+            .css("div.panel-body")
+            .css("span.label::text")
+            .extract_first()
+        )
 
-        rating = response.css('div.col-lg-5').css('div.panel-heading').css('span.label')[0].css('span.label::text').extract()[0]
+        rating = (
+            response.css("div.col-lg-5")
+            .css("div.panel-heading")
+            .css("span.label")[0]
+            .css("span.label::text")
+            .extract()[0]
+        )
 
-        nationality = slugify(response.css('h2.subtitle a::text').extract()[0])
+        nationality = slugify(response.css("h2.subtitle a::text").extract()[0])
 
         yield {
-            'name': slugify(name),
-            'info': {
-                'raw team': team,
-                'team': slugify(team),
-                'position': position,
-                'raw name': name,
-                'rating': int(rating),
-                'kit number': number,
-                'nationality': nationality,
-                'url': response.request.url,
-            }
+            "name": slugify(name),
+            "info": {
+                "raw team": team,
+                "team": slugify(team),
+                "position": position,
+                "raw name": name,
+                "rating": int(rating),
+                "kit number": number,
+                "nationality": nationality,
+                "url": response.request.url,
+            },
         }
 
 
@@ -77,80 +110,101 @@ class MatchSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            # 'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/results/2017-2018/',
-            # 'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/results/2016-2017/',
-            # 'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/results/2015-2016/',
-            # 'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/results/2014-2015/',
-            # 'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/results/2013-2014/',
-            'http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2017-2018/',
-            'http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2016-2017/',
-            'http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2015-2016/',
-            'http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2014-2015/',
-            'http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2013-2014/'
-
+            "http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2017-2018/",
+            "http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2016-2017/",
+            "http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2015-2016/",
+            "http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2014-2015/",
+            "http://www.betstudy.com/soccer-stats/c/france/ligue-1/d/results/2013-2014/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_fixtures_page)
 
     def parse_fixtures_page(self, response):
-        for info_button in response.css('ul.action-list').css('a::attr(href)'):
+        for info_button in response.css("ul.action-list").css("a::attr(href)"):
             url = response.urljoin(info_button.extract())
             yield scrapy.Request(url, callback=self.parse_match_page)
 
     def parse_match_page(self, response):
-        
-        home_team, away_team = response.css('div.player h2 a::text').extract()
 
-        date = response.css('em.date').css('span.timestamp::text').extract_first()
+        home_team, away_team = response.css("div.player h2 a::text").extract()
+
+        date = response.css("em.date").css("span.timestamp::text").extract_first()
 
         url = response.request.url
 
-        match_number = response.request.url.split('-')[-1].split('/')[0]
+        match_number = response.request.url.split("-")[-1].split("/")[0]
 
-        home_goals, away_goals = response.css('div.info strong.score::text').extract_first().split('-')
-        
-        for table in response.css('div.table-holder'):
-            if table.css('h2::text').extract_first() == 'Lineups and subsitutes':
+        home_goals, away_goals = (
+            response.css("div.info strong.score::text").extract_first().split("-")
+        )
+
+        for table in response.css("div.table-holder"):
+            if table.css("h2::text").extract_first() == "Lineups and subsitutes":
                 lineups = table
-        
-        home_lineup_css = lineups.css('table.info-table')[0]
-        away_lineup_css = lineups.css('table.info-table')[1]
 
-        home_lineup_raw = [slugify(x) for x in home_lineup_css.css('tr td.left-align').css('a::attr(title)').extract()]
-        away_lineup_raw = [slugify(x) for x in away_lineup_css.css('tr td.left-align').css('a::attr(title)').extract()]
+        home_lineup_css = lineups.css("table.info-table")[0]
+        away_lineup_css = lineups.css("table.info-table")[1]
 
-        home_lineup = [slugify(x) for x in home_lineup_css.css('tr td.left-align').css('a::text').extract()]
-        away_lineup = [slugify(x) for x in away_lineup_css.css('tr td.left-align').css('a::text').extract()]
+        home_lineup_raw = [
+            slugify(x)
+            for x in home_lineup_css.css("tr td.left-align")
+            .css("a::attr(title)")
+            .extract()
+        ]
+        away_lineup_raw = [
+            slugify(x)
+            for x in away_lineup_css.css("tr td.left-align")
+            .css("a::attr(title)")
+            .extract()
+        ]
 
-        home_lineup_number = [int(x) for x in home_lineup_css.css('tr td.size23 strong::text').extract()]
-        away_lineup_number = [int(x) for x in away_lineup_css.css('tr td.size23 strong::text').extract()]
+        home_lineup = [
+            slugify(x)
+            for x in home_lineup_css.css("tr td.left-align").css("a::text").extract()
+        ]
+        away_lineup = [
+            slugify(x)
+            for x in away_lineup_css.css("tr td.left-align").css("a::text").extract()
+        ]
 
-        home_lineup_nationality = [int(x) for x in home_lineup_css.css('tr td.left-align::attr(alt)').extract()]
-        away_lineup_nationality = [int(x) for x in away_lineup_css.css('tr td.left-align::attr(alt)').extract()]
+        home_lineup_number = [
+            int(x) for x in home_lineup_css.css("tr td.size23 strong::text").extract()
+        ]
+        away_lineup_number = [
+            int(x) for x in away_lineup_css.css("tr td.size23 strong::text").extract()
+        ]
 
-        home_lineup_nationality = [slugify(x) for x in
-                                   home_lineup_css.css('tr td.left-align').css('img.flag-ico::attr(alt)').extract()]
-        away_lineup_nationality = [slugify(x) for x in
-                                   away_lineup_css.css('tr td.left-align').css('img.flag-ico::attr(alt)').extract()]
+        home_lineup_nationality = [
+            slugify(x)
+            for x in home_lineup_css.css("tr td.left-align")
+            .css("img.flag-ico::attr(alt)")
+            .extract()
+        ]
+        away_lineup_nationality = [
+            slugify(x)
+            for x in away_lineup_css.css("tr td.left-align")
+            .css("img.flag-ico::attr(alt)")
+            .extract()
+        ]
 
         yield {
-            'match number': int(match_number),
-            'info': {
-                'date': date,
-                'home team': slugify(home_team),
-                'away team': slugify(away_team),
-                'home goals': int(home_goals),
-                'away goals': int(away_goals),
-                'home lineup raw names': home_lineup_raw,
-                'away lineup raw names': away_lineup_raw,
-                'home lineup names': home_lineup,
-                'away lineup names': away_lineup,
-                'home lineup numbers': home_lineup_number,
-                'away lineup numbers': away_lineup_number,
-                'home lineup nationalities': home_lineup_nationality,
-                'away lineup nationalities': away_lineup_nationality,
-                'url': url,
-            }
+            "match number": int(match_number),
+            "info": {
+                "date": date,
+                "home team": slugify(home_team),
+                "away team": slugify(away_team),
+                "home goals": int(home_goals),
+                "away goals": int(away_goals),
+                "home lineup raw names": home_lineup_raw,
+                "away lineup raw names": away_lineup_raw,
+                "home lineup names": home_lineup,
+                "away lineup names": away_lineup,
+                "home lineup numbers": home_lineup_number,
+                "away lineup numbers": away_lineup_number,
+                "home lineup nationalities": home_lineup_nationality,
+                "away lineup nationalities": away_lineup_nationality,
+                "url": url,
+            },
         }
 
 
@@ -161,46 +215,82 @@ class FifaIndexTeamScraper(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.fifaindex.com/teams/',
-            'https://www.fifaindex.com/teams/fifa17_173/',
-            'https://www.fifaindex.com/teams/fifa16_73/',
-            'https://www.fifaindex.com/teams/fifa15_14/',
-            'https://www.fifaindex.com/teams/fifa14_13/',
+            "https://www.fifaindex.com/teams/",
+            "https://www.fifaindex.com/teams/fifa17_173/",
+            "https://www.fifaindex.com/teams/fifa16_73/",
+            "https://www.fifaindex.com/teams/fifa15_14/",
+            "https://www.fifaindex.com/teams/fifa14_13/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        links = [a.extract() for a in response.css('td a::attr(href)')]
+        links = [a.extract() for a in response.css("td a::attr(href)")]
         for link in links:
-            if '/team/' in link:
+            if "/team/" in link:
                 url = response.urljoin(link)
                 yield scrapy.Request(url, callback=self.parse_team)
 
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None and int(next_page.split('/')[-2]) < 10:
+        next_page = response.css("li.next a::attr(href)").extract_first()
+        if next_page is not None and int(next_page.split("/")[-2]) < 10:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
-    def parse_team(self, response):
-        team = slugify(response.css('.media-heading::text').extract_first())
+    @staticmethod
+    def parse_team(response):
+        team = slugify(response.css(".media-heading::text").extract_first())
 
         for i in range(1, len(response.css('tr'))):
-            name = slugify(response.css('.table > tbody:nth-child(2) > tr:nth-child({}) > td:nth-child(6) > a:nth-child(1)::attr(title)'.format(i)).extract_first())
-            number = int(response.css('.table > tbody:nth-child(2) > tr:nth-child({}) > td:nth-child(1)::text'.format(i)).extract_first())
-            nationality = slugify(response.css('.table > tbody:nth-child(2) > tr:nth-child({}) > td:nth-child(4) > a:nth-child(1) > img:nth-child(1)::attr(title)'.format(i)).extract_first())
-            position = response.css('.table > tbody:nth-child(2) > tr:nth-child({}) > td:nth-child(7) > a:nth-child(1) > span:nth-child(1)::text'.format(i)).extract_first()
-            rating = response.css('table > tbody:nth-child(2) > tr:nth-child({}) > td:nth-child(5) > span:nth-child(1)::text'.format(i)).extract_first()
+            name_css = (
+                ".table > "
+                "tbody:nth-child(2) > "
+                "tr:nth-child({}) > "
+                "td:nth-child(6) > "
+                "a:nth-child(1)::attr(title)"
+            )
+            name = slugify(response.css(name_css.format(i)).extract_first())
 
+            number_css = (
+                ".table > " "tbody:nth-child(2) > " "tr:nth-child({}) > " "td:nth-child(1)::t" "ext"
+            )
+            number = int(response.css(number_css.format(i)).extract_first())
+
+            nationality_css = (
+                ".table > "
+                "tbody:nth-child(2) > "
+                "tr:nth-child({}) > "
+                "td:nth-child(4) > "
+                "a:nth-child(1) > img:nth-child(1)::attr(title)"
+            )
+            nationality = slugify(
+                response.css(nationality_css.format(i)).extract_first())
+
+            position_css = (
+                ".table > "
+                "tbody:nth-child(2) > "
+                "tr:nth-child({}) > "
+                "td:nth-child(7) > "
+                "a:nth-child(1) > span:nth-child(1)::text"
+            )
+            position = response.css(position_css.format(i)).extract_first()
+
+            rating_css = (
+                "table > t"
+                "body:nth-child(2) > t"
+                "r:nth-child({}) > t"
+                "d:nth-child(5) > s"
+                "pan:nth-child(1)::text"
+            )
+            rating = response.css(rating_css.format(i)).extract_first()
 
             yield {
-                'name': slugify(name),
-                'team': team,
-                'position': position,
-                'rating': int(rating),
-                'number': number,
-                'nationality': nationality,
-                'url': response.request.url
+                "name": slugify(name),
+                "team": team,
+                "position": position,
+                "rating": int(rating),
+                "number": number,
+                "nationality": nationality,
+                "url": response.request.url,
             }
 
 
@@ -211,20 +301,20 @@ class FixturesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://www.betstudy.com/soccer-stats/c/england/premier-league/d/fixtures/'
+            "http://www.betstudy.com/soccer-stats/c/england/premier-league/d/fixtures/"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_fixtures)
 
-    def parse_fixtures(self, response):
-        for fixture in response.css('tr')[1:]:
-            home_team = fixture.css('td.right-align a::text').extract_first()
-            away_team = fixture.css('td.left-align a::text').extract_first()
-            date = fixture.css('td::text').extract_first()
+    @staticmethod
+    def parse_fixtures(response):
+        for fixture in response.css("tr")[1:]:
+            home_team = fixture.css("td.right-align a::text").extract_first()
+            away_team = fixture.css("td.left-align a::text").extract_first()
+            date = fixture.css("td::text").extract_first()
             yield {
-                'date': date,
-                'home team': slugify(home_team),
-                'away team': slugify(away_team),
-                'url': response.request.url
+                "date": date,
+                "home team": slugify(home_team),
+                "away team": slugify(away_team),
+                "url": response.request.url,
             }
-
--- fifa_ratings_predictor/data_methods.py
+++ fifa_ratings_predictor/data_methods.py
@@ -1,69 +1,77 @@
+import datetime
 import glob
 import json
-import datetime
-
-import pandas as pd
-import numpy as np
-from slugify import slugify
 
 import fifa_ratings_predictor.constants as constants
+import numpy as np
+import pandas as pd
+from slugify import slugify
 
 
 def read_player_data(season=None):
-    with open('./data/player-data/players-by-team.json') as json_file:
+    with open("./data/player-data/players-by-team.json") as json_file:
         data = json.load(json_file)
 
     data = assign_guids(data)
 
     for _, player in data.items():
-        player['general position'] = assign_general_position(player['position'])
-        player['season'] = assign_season_to_player(player['url'])
+        player["general position"] = assign_general_position(player["position"])
+        player["season"] = assign_season_to_player(player["url"])
 
     if season is not None:
-        data = {guid: player_details for guid, player_details in data.items() if player_details['season'] == season}
+        data = {
+            guid: player_details
+            for guid, player_details in data.items()
+            if player_details["season"] == season
+        }
 
     assert data, "No match lineups to return, have you selected a valid season?"
 
     return data
 
 
-def read_match_data(season=None, sort=True, league='E0'):
-    with open('./data/lineup-data/' + league + '/match-lineups.json') as json_file:
+def read_match_data(season=None, sort=True, league="E0"):
+    filen = "./data/lineup-data/" + league + "/match-lineups.json"
+    with open(filen) as json_file:
         data = json.load(json_file)
 
     for match in data:
-        match['info']['season'] = assign_season_to_match(match['info']['date'])
+        match["info"]["season"] = assign_season_to_match(match["info"]["date"])
 
     for match in data:
-        match['info']['season'] = assign_season_to_match(match['info']['date'])
+        match["info"]["season"] = assign_season_to_match(match["info"]["date"])
 
     if season is not None:
-        data = [match for match in data if match['info']['season'] == season]
+        data = [match for match in data if match["info"]["season"] == season]
 
     if sort:
         for match in data:
-            match['info']['datetime'] = convert_date_to_datetime_object(match['info']['date'])
-        data = sorted(data, key=lambda x: x['info']['datetime'])
+            match["info"]["datetime"] = convert_date_to_datetime_object(
+                match["info"]["date"]
+            )
+        data = sorted(data, key=lambda x: x["info"]["datetime"])
 
     assert data, "No match lineups to return, have you selected a valid season?"
 
     return data
 
 
-def read_fixtures_data(filepath='./data/fixtures/E0/18-19-fixtures.json'):
+def read_fixtures_data(filepath="./data/fixtures/E0/18-19-fixtures.json"):
     with open(filepath) as jsonfile:
         fixtures = json.load(jsonfile)
 
     for fixture in fixtures:
-        fixture['datetime'] = convert_date_to_datetime_object(fixture['date'], string_format='%d.%m.%Y')
+        fixture["datetime"] = convert_date_to_datetime_object(
+            fixture["date"], string_format="%d.%m.%Y"
+        )
 
-    fixtures = sorted(fixtures, key=lambda x: x['datetime'])
+    fixtures = sorted(fixtures, key=lambda x: x["datetime"])
 
     return fixtures
 
 
 def read_all_football_data(league):
-    path = './data/football-data/' + league
+    path = "./data/football-data/" + league
     all_files = glob.glob(path + "/*.csv")
     list_ = []
     for file_ in all_files:
@@ -71,8 +79,8 @@ def read_all_football_data(league):
         list_.append(df)
     df = pd.concat(list_, sort=False)
 
-    df = df[~df['HomeTeam'].isnull()]
-    df = df[~df['AwayTeam'].isnull()]
+    df = df[~df["HomeTeam"].isnull()]
+    df = df[~df["AwayTeam"].isnull()]
 
     return df
 
@@ -82,7 +90,7 @@ def normalise_features(vector):
     return ((vector - 50) / (100 - 50)).clip(min=0)
 
 
-def convert_date_to_datetime_object(date, string_format='%d %B %Y'):
+def convert_date_to_datetime_object(date, string_format="%d %B %Y"):
     return datetime.datetime.strptime(date, string_format)
 
 
@@ -93,22 +101,22 @@ def assign_season_to_match(date):
     year = date.year
     month = date.month
     if month in [7, 8, 9, 10, 11, 12]:
-        season = str(year) + '-' + str(year + 1)
+        season = str(year) + "-" + str(year + 1)
     else:
-        season = str(year - 1) + '-' + str(year)
+        season = str(year - 1) + "-" + str(year)
     return season
 
 
 def assign_season_to_player(url):
-    url = url.split('/')[-2]
-    season = constants.PLAYER_URL_TO_SEASON.get(url, '2017-2018')
+    url = url.split("/")[-2]
+    season = constants.PLAYER_URL_TO_SEASON.get(url, "2017-2018")
     return season
 
 
 def assign_guids(data):
     for i, player in enumerate(data):
-        player['guid'] = i
-    guid_conversion = {player['guid']: player for player in data}
+        player["guid"] = i
+    guid_conversion = {player["guid"]: player for player in data}
     return guid_conversion
 
 
@@ -117,11 +125,15 @@ def assign_general_position(position):
 
 
 def assign_odds_to_match(matchlineups, fd):
-    league = fd['Div'].tolist()[0]
+    league = fd["Div"].tolist()[0]
     for match in matchlineups:
         try:
-            home_team = constants.FOOTBALL_DATA_TEAM_MAPPINGS[league][match['info']['home team']]
-            away_team = constants.FOOTBALL_DATA_TEAM_MAPPINGS[league][match['info']['away team']]
+            home_team = constants.FOOTBALL_DATA_TEAM_MAPPINGS[league][
+                match["info"]["home team"]
+            ]
+            away_team = constants.FOOTBALL_DATA_TEAM_MAPPINGS[league][
+                match["info"]["away team"]
+            ]
 
         except KeyError:
             home_team = None
@@ -129,40 +141,59 @@ def assign_odds_to_match(matchlineups, fd):
 
         for index, row in fd.iterrows():
 
-            if home_team == slugify(row['HomeTeam']) and away_team == slugify(row['AwayTeam']):
-                if datetime.datetime.strptime(match['info']['date'], '%d %B %Y') == datetime.datetime.strptime(row['Date'], '%d/%m/%y'):
-                    match['info']['home odds'] = row['PSH']
-                    match['info']['draw odds'] = row['PSD']
-                    match['info']['away odds'] = row['PSA']
+            if home_team == slugify(row["HomeTeam"]) and away_team == slugify(
+                row["AwayTeam"]
+            ):
+                if datetime.datetime.strptime(
+                    match["info"]["date"], "%d %B %Y"
+                ) == datetime.datetime.strptime(row["Date"], "%d/%m/%y"):
+                    match["info"]["home odds"] = row["PSH"]
+                    match["info"]["draw odds"] = row["PSD"]
+                    match["info"]["away odds"] = row["PSA"]
                     break
 
     return matchlineups
 
 
 def get_goals(match):
-    return match['info']['home goals'], match['info']['away goals']
+    return match["info"]["home goals"], match["info"]["away goals"]
 
 
 def get_season(match):
-    return match['info']['season']
+    return match["info"]["season"]
 
 
 def get_lineup_names(match):
-    return match['info']['home lineup names'], match['info']['away lineup names']
+    return (
+        match["info"]["home lineup names"],
+        match["info"]["away lineup names"],
+    )
 
 
 def get_teams(match):
-    return constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][match['info']['home team']], \
-           constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS['ALL'][match['info']['away team']]
+    return (
+        constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS["ALL"][match["info"]["home team"]],
+        constants.LINEUP_TO_PLAYER_TEAM_MAPPINGS["ALL"][match["info"]["away team"]],
+    )
 
 
 def get_lineup_numbers(match):
-    return match['info']['home lineup numbers'], match['info']['away lineup numbers']
+    return (
+        match["info"]["home lineup numbers"],
+        match["info"]["away lineup numbers"],
+    )
 
 
 def get_lineup_nationalities(match):
-    return match['info']['home lineup nationalities'], match['info']['away lineup nationalities']
+    return (
+        match["info"]["home lineup nationalities"],
+        match["info"]["away lineup nationalities"],
+    )
 
 
 def get_match_odds(match):
-    return match['info']['home odds'], match['info']['draw odds'], match['info']['away odds']
+    return (
+        match["info"]["home odds"],
+        match["info"]["draw odds"],
+        match["info"]["away odds"],
+    )
